0c971322f9e48a2714a1457239619d57e9ee1883
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2025-12-31 13:07:41 +0600

    Create While lop 1 to N print.c

---
+++ While lop 1 to N print.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    
+    int i = 1,N;
+    
+    printf("Enter Your Number : ");
+    scanf("%d",&N);
+    
+    while(i <= N){
+        printf("%d\n",i);
+        i++;
+    }
+    
+    return 0;
+}
